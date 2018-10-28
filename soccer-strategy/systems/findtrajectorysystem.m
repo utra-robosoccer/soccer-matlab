5169857c6ddcf16a26f41aec07b13eac4af850ea
John Doe <vuwij@me.com>
2018-10-29 00:50:57 +0900

    Fixed ikine bug, added repeat test.m for faster testing

--- soccer-strategy/systems/findtrajectorysystem.m
+++ soccer-strategy/systems/findtrajectorysystem.m
@@ -1,20 +1,22 @@
-classdef findtrajectorysystem < matlab.System
+classdef findtrajectorysystem < matlab.System & matlab.system.mixin.Propagates
 
     properties
-
+        outputduration = 100;            % Seconds
     end
 
     properties(DiscreteState)
-
     end
 
     properties(Access = private)
         robot;
+        trajectory;
     end
-
+    
     methods(Access = protected)
         function setupImpl(obj)
-            obj.robot = Navigation.Robot(Pose(0,0,0,0,0), Navigation.EntityType.Self, 0.10);
+            obj.robot = Navigation.Robot(Pose(0,0,0,0,0), Navigation.EntityType.Self, 0.05);
+            obj.trajectory = zeros(10000,20);    % Output max trajectory
+
         end
 
         function trajectoryOut = stepImpl(obj, currentPose, destinationPose, obstacles)
@@ -23,7 +25,7 @@ classdef findtrajectorysystem < matlab.System
             % obstacles = [1xN] Pose
             
             % Test
-            destPose = Pose(2.5,2.5,10,0,0);
+            endPose = Pose(2.5,2.5,10,0,0);
 
             % Add objstacles
             obs1 = Navigation.Entity(Pose(1.3,1.3,0,0,0), Navigation.EntityType.Friendly);
@@ -31,10 +33,31 @@ classdef findtrajectorysystem < matlab.System
             obs3 = Navigation.Entity(Pose(1.5,-1.5,0,0,0), Navigation.EntityType.Friendly);
 
             map = Navigation.Map(9, 6, 0.05);
-            map.objects = {robot, obs1, obs2, obs3};
-%             trajectory = map.FindTrajectory(robot.pose, endPose, robot.speed);
-%             
-             trajectoryOut = zeros(3000,20);
+            map.objects = {obj.robot, obs1, obs2, obs3};
+            traj = map.FindTrajectory(obj.robot.pose, endPose, obj.robot.speed);
+            
+            % Fill in the trajectory
+            [l,w] = size(traj.angles);
+            for i = 1:w
+                for j = 1:l
+                    obj.trajectory(i,j) = traj.angles(j,i);
+                end
+            end
+            
+            trajectoryOut = obj.trajectory;
+        end
+        
+        function c1 = isOutputFixedSizeImpl(~)
+            c1 = true;
+        end
+        function sz_1 = getOutputSizeImpl(obj)
+            sz_1 = [obj.outputduration * 100 20];
+        end 
+        function out = getOutputDataTypeImpl(~)
+            out = "double";
+        end
+        function c1 = isOutputComplexImpl(~)
+            c1 = false;
         end
     end
 end
