01d2a9eb8400746973730547cf27a0f0c0aa8c86
Pratham Desai <prathamdesai13@gmail.com>
2018-11-12 23:37:27 -0500

    Naive solution

--- soccer-strategy/test/test_pathfinding.m
+++ soccer-strategy/test/test_pathfinding.m
@@ -13,8 +13,15 @@ obs3 = Navigation.Entity(Pose(1.5,-1.5,0,0,0), Navigation.EntityType.Friendly);
 
 map = Navigation.Map(9, 6, 0.05);
 map.objects = {robot, obs1, obs2, obs3};
+<<<<<<< HEAD
 trajectory = map.FindTrajectory(robot, endPose, robot.speed);
 
+=======
+trajectory = map.FindTrajectory(robot.pose, endPose, robot.speed);
+for i = 1:length(trajectory.waypoints)
+    disp(trajectory.poseactions{i}.ActionLabel);
+end
+>>>>>>> Naive solution
 % Draw the angles
 % figure;
 % trajectory.PlotAngles();
@@ -28,4 +35,37 @@ hold on;
 grid minor;
 map.Draw();
 trajectory.DrawPath();
+<<<<<<< HEAD
 plot(simPose(:,1), simPose(:,2));
+=======
+
+plot(truepath(:,1), truepath(:,2));
+
+legend('Expected Path', 'Simulation Ground Truth')
+
+%figure;
+%trajectory.PlotAngles();
+%trajectory.AverageSpeed();
+%robot.SimulationTrajectory(trajectory);
+% 
+% for i = 1:length(trajectory.waypoints)
+%     disp(trajectory.poseactions{i}.ActionLabel);
+% end
+
+% Draw the map and path
+map.Draw();
+hold on;
+trajectory.DrawPath();
+%figure;
+%trajectory.PlotAngles();
+%trajectory.AverageSpeed();
+robot.SimulationTrajectory(trajectory);
+plot(simPose(:,1), simPose(:,2));
+% map.Draw();
+% hold on;
+% trajectory.DrawPath();
+% %figure;
+% %trajectory.PlotAngles();
+% %trajectory.AverageSpeed();
+% robot.SimulationTrajectory(trajectory);
+>>>>>>> Naive solution
