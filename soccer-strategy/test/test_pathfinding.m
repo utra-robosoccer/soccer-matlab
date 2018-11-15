711d1b907ef6d8b412fd7b49a128bdcdd907a37f
Pratham Desai <prathamdesai13@gmail.com>
2018-11-15 17:12:44 -0500

    pathfinding

--- soccer-strategy/test/test_pathfinding.m
+++ soccer-strategy/test/test_pathfinding.m
@@ -12,7 +12,7 @@ obs2 = Navigation.Entity(Pose(-1.7,1.7,0,0,0), Navigation.EntityType.Friendly);
 obs3 = Navigation.Entity(Pose(1.5,-1.5,0,0,0), Navigation.EntityType.Friendly);
 
 map = Navigation.Map(9, 6, 0.05);
-map.objects = {robot, obs1, obs2, obs3}; 
+map.objects = {robot, obs1, obs2, obs3};
 trajectory = map.FindTrajectory(robot, endPose, robot.speed);
 
 % Draw the angles
