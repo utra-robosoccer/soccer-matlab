bee2463bd7759aa6fa09eb91fac008b2bf2c77a1
Daniel <daniel.campoverdevarela@mail.utoronto.ca>
2018-11-24 20:37:12 -0500

    changed back motor calibration value
    it seems like the calibration is been applied twice
    fixed and formatted feedback control in simulink

--- soccer-strategy/soccer_strategy_setup.m
+++ soccer-strategy/soccer_strategy_setup.m
@@ -17,7 +17,7 @@ sampleTime = 0.01;
 motorCalibration = [ ...
     0; ... % Torso              -    Right Hip (Side)
     0; ... % Right Hip (Side)   -    Right Hip (Front)
-    0.09;...0.09; ... % Right Hip (Front)  -    Right Thigh
+    0;...0.09; ... % Right Hip (Front)  -    Right Thigh
     0; ... % Right Thigh        -    Right Calve
     0; ... % Right Calve        -    Right Ankle
     0; ... % Right Ankle        -    Right Foot
@@ -44,5 +44,6 @@ hipCalveGain = 1;
 kneeGain = 2;
 
 % Forward Backward control
+hipGain = 1;
 forearmGain = 1.5;
 feetGain = 0;
